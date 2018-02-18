b985fe34c930ad246466c993580de8b695b0468c
dougwyu <dougwyu@gmail.com>
2018-02-18 19:51:44 +0000

    added code to run the pardosa_glacialis_only scripts

--- 3_minimap2_samtools/launch_bsub_PlatesGH.sh
+++ 3_minimap2_samtools/launch_bsub_PlatesGH.sh
@@ -28,8 +28,8 @@ PATH=$PATH:~/scripts/parallel-20170722/bin/
 # copy the minimap and samtools shell and bsub scripts into each BWA folder
 cd ~/greenland_2017/platesGH/platesGH_combined/
 ls
-parallel cp loop_minimap2_only_20180216.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-parallel cp _loop_minimap2_only_20180216.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp loop_samtools_only_20180217_Pardosa_glacialis.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp _loop_samtools_only_20180217_Pardosa_glacialis.sh BWA{} ::: 02 04 05 06 07 08 09 10
 # parallel cp loop_samtools_only_20180210.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp _loop_samtools_only_20180210.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp loop_trimgalore_20180216.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
@@ -42,48 +42,43 @@ parallel "sed 's/mnmploop01/mnmploop{}/g' BWA{}/loop_minimap2_only_20180216.bsub
 parallel "mv BWA{}/loop_minimap2_only_20180216_tmp.bsub BWA{}/loop_minimap2_only_20180216.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180216.bsub # check.  should be mnmploop10
 
-# parallel "sed 's/samtools01/samtools{}/g' BWA{}/loop_samtools_only_20180210.bsub > BWA{}/loop_samtools_only_20180210_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-# parallel "mv BWA{}/loop_samtools_only_20180210_tmp.bsub BWA{}/loop_samtools_only_20180210.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-# ls # BWA* folders should now sort to bottom
-# head BWA{01,02,03,04,05,10}/loop_samtools_only_20180210.bsub # check.  should be mnmploop10
+parallel "sed 's/samtools01/samtools{}/g' BWA{}/loop_samtools_only_20180217_Pardosa_glacialis.bsub > BWA{}/loop_samtools_only_20180217_Pardosa_glacialis_tmp.bsub" ::: 02 04 05 06 07 08 09 10
+parallel "mv BWA{}/loop_samtools_only_20180217_Pardosa_glacialis_tmp.bsub BWA{}/loop_samtools_only_20180217_Pardosa_glacialis.bsub" ::: 02 04 05 06 07 08 09 10
+ls # BWA* folders should now sort to bottom
+head BWA{02,04,05,06,07,08,09,10}/loop_samtools_only_20180217_Pardosa_glacialis.bsub # check.  should be mnmploop10
+tail BWA{02,04,05,06,07,08,09,10}/loop_samtools_only_20180217_Pardosa_glacialis.bsub # check.  should be mnmploop10
 
 # parallel "sed 's/trimgal01/trimgal{}/g' BWA{}/loop_trimgalore_20180216.bsub > BWA{}/loop_trimgalore_20180216_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "mv BWA{}/loop_trimgalore_20180216_tmp.bsub BWA{}/loop_trimgalore_20180216.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 # head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_trimgalore_20180216.bsub # check job names
 
 ####### launch samtools scripts #######
-cd ~/greenland_2017/platesGH/platesGH_combined/BWA01; ls
-bsub < loop_trimgalore_20180216.bsub
-bjobs | sort -k8
-
+# skipping BWA01 and BWA03 because minimap2 didn't finish
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA02; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 bjobs | sort -k8
-
-cd ~/greenland_2017/platesGH/platesGH_combined/BWA03; ls
-bsub < loop_trimgalore_20180216.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA04; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 bjobs | sort -k8
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA05; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA06; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA07; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA08; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA09; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 
 cd ~/greenland_2017/platesGH/platesGH_combined/BWA10; ls
-bsub < loop_trimgalore_20180216.bsub
+bsub < loop_samtools_only_20180217_Pardosa_glacialis.bsub
 bjobs | sort -k8
 
 
