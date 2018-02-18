10e2b6c387f4cd9e5f57ec9fe888983b013ac217
dougwyu <dougwyu@gmail.com>
2018-02-18 19:51:25 +0000

    update to 20180218 versions of bsub and minimap.sh files

--- 3_minimap2_samtools/launch_bsub_PlatesEF.sh
+++ 3_minimap2_samtools/launch_bsub_PlatesEF.sh
@@ -10,9 +10,9 @@ set -o pipefail
 
 # upload the new samtools bsub file from macOS
 # run in macOS, not hpc
-scp ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/mapping_git/3_minimap2_samtools/loop_minimap2_only_20180216.bsub b042@hpc.uea.ac.uk:~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed
+scp ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/mapping_git/3_minimap2_samtools/loop_minimap2_only_20180218.bsub b042@hpc.uea.ac.uk:~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed
 
-scp ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/mapping_git/3_minimap2_samtools/_loop_minimap2_only_20180216.sh b042@hpc.uea.ac.uk:~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed
+scp ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/mapping_git/3_minimap2_samtools/_loop_minimap2_only_20180218.sh b042@hpc.uea.ac.uk:~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed
 
 ssh hpc
 interactive
@@ -28,8 +28,8 @@ PATH=$PATH:~/scripts/parallel-20170722/bin/
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed; ls
 ls # loop_samtools_only_20180210.bsub and _loop_samtools_only_20180210.sh should sort to bottom
 
-parallel cp loop_minimap2_only_20180216.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-parallel cp _loop_minimap2_only_20180216.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp loop_minimap2_only_20180218.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp _loop_minimap2_only_20180218.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp loop_samtools_only_20180210.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp _loop_samtools_only_20180210.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp loop_trimgalore_20180216.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
@@ -38,9 +38,10 @@ ls
 
 # edit the bsub files so that the correct job name will show up (i suppose i could have instead run a job array...)
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed; ls
-parallel "sed 's/mnmploop01/mnmploop{}/g' BWA{}/loop_minimap2_only_20180216.bsub > BWA{}/loop_minimap2_only_20180216_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-parallel "mv BWA{}/loop_minimap2_only_20180216_tmp.bsub BWA{}/loop_minimap2_only_20180216.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180216.bsub # check.  should be mnmploop10
+parallel "sed 's/mnmploop01/mnmploop{}/g' BWA{}/loop_minimap2_only_20180218.bsub > BWA{}/loop_minimap2_only_20180218_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
+parallel "mv BWA{}/loop_minimap2_only_20180218_tmp.bsub BWA{}/loop_minimap2_only_20180218.bsub" ::: 01 02 03 04 05 06 07 08 09 10
+head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
+tail -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
 
 # parallel "sed 's/samtools01/samtools{}/g' BWA{}/loop_samtools_only_20180210.bsub > BWA{}/loop_samtools_only_20180210_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "mv BWA{}/loop_samtools_only_20180210_tmp.bsub BWA{}/loop_samtools_only_20180210.bsub" ::: 01 02 03 04 05 06 07 08 09 10
@@ -123,52 +124,52 @@ bjobs | sort -k8
 
 ############# launch minimap2 scripts #############
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA01; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA02; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA03; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA04; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA05; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA06; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA07; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA08; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA09; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
 cd ~/greenland_2017/platesEF_Earlham_soups_20170603/Earlham_soups_20170603_fastq_combine/fastqc_completed/BWA10; ls
-bsub < loop_minimap2_only_20180216.bsub
+bsub < loop_minimap2_only_20180218.bsub
 bjobs
 ls
 
