6b10ea6b4bab0bd1fd39e8f274665eafaa6ed5a8
dougwyu <dougwyu@gmail.com>
2018-02-18 21:12:13 +0000

    added a loop to bold_identify_parents one at a time

--- 5_taxonomy/bold_identify_macOS.R
+++ 5_taxonomy/bold_identify_macOS.R
@@ -9,13 +9,12 @@ library("readr")
 ## bold_identify multiple sequences from a fasta file ####
 
 #### load a fasta file
-# testseqsall <- read.fasta(file = "CO1_1sequence_perBIN_040915_COIspiking.fas", seqtype = "DNA", set.attributes = FALSE, as.string = TRUE, forceDNAtolower = FALSE)  # read in fasta file using seqinr
+testseqsall <- read.fasta(file = "CO1_1sequence_perBIN_040915_COIspiking.fas", seqtype = "DNA", set.attributes = FALSE, as.string = TRUE, forceDNAtolower = FALSE)  # read in fasta file using seqinr
 
-testseqsall <- read.fasta(file = "CO1_1sequence_perBIN_040915_COIspiking_305_to_413.fas", seqtype = "DNA", set.attributes = FALSE, as.string = TRUE, forceDNAtolower = FALSE)  # read in fasta file using seqinr
+# testseqsall <- read.fasta(file = "GMGLM411_13.fas", seqtype = "DNA", set.attributes = FALSE, as.string = TRUE, forceDNAtolower = FALSE)  # read in fasta file using seqinr
 
 #### bold_identify all sequences at once
 testseqsallbold <- bold_identify(testseqsall, db = "COX1")
-# 
 # #### bold_identify one sequence at a time (for large files that take a long time and might crash partway, this saves the output of each sequence)
 # testseqsallbold <- vector('list')
 # for (i in 1:length(testseqsall)) {
@@ -30,6 +29,13 @@ testseqsallbold <- testseqsallbold[!sapply(testseqsallbold, is.null)]
 
 #### add parent taxa to output, wide format (another call to BOLD and will also take time but should be faster)
 testseqsallbold_parents <- bold_identify_parents(testseqsallbold, wide = TRUE)
+# #### bold_identify_parents one sequence at a time (for large files that take a long time and might crash partway, this saves the output of each sequence)
+# testseqsallbold_parents <- vector('list')
+# for (i in 1:length(testseqsallbold)) {
+#     print(i)
+#     boldoutput <- bold_identify_parents(testseqsallbold[i], db = "COX1")
+#     testseqsallbold_parents <- c(testseqsallbold_parents, boldoutput)
+# }
 
 #### convert list to dataframe
 testseqsallbold_parents.df <- ldply(testseqsallbold_parents, data.frame)
@@ -41,7 +47,5 @@ testseqsallbold_parents_96_to_100.df <- dplyr::filter(testseqsallbold_parents.df
 testseqsallbold_parents_96_to_100.df <- dplyr::arrange(testseqsallbold_parents_96_to_100.df, .id, desc(similarity)) # .id is the name of the sequence that was identified
 
 #### write dataframe to disk
-write_tsv(testseqsallbold_parents_96_to_100.df, "CO1_1sequence_perBIN_040915_COIspiking_96_to_100.tsv")
+write_tsv(testseqsallbold_parents_96_to_100_413seqs.df, "CO1_1sequence_perBIN_040915_COIspiking_96_to_100.tsv")
 write_tsv(testseqsallbold_parents.df, "CO1_1sequence_perBIN_040915_COIspiking.tsv")
-
-
