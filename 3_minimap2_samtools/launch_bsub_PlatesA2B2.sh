5f2b33ac3cc0dea7e3143f20fa9cdd2678b826a7
dougwyu <dougwyu@gmail.com>
2018-02-22 19:09:33 +0000

    generalise to use variable for minimap2 sh and bsub filenames

--- 3_minimap2_samtools/launch_bsub_PlatesA2B2.sh
+++ 3_minimap2_samtools/launch_bsub_PlatesA2B2.sh
@@ -26,36 +26,89 @@ PATH=$PATH:~/scripts/parallel-20170722/bin/
 ############## by hand, copy 1/10 the sample files into each BWA folder
 
 ############# copy the minimap and samtools shell and bsub scripts into each BWA folder
-SAMTOOLS_BSUB="loop_samtools_only_20180219.bsub"; echo ${SAMTOOLS_BSUB}
-SAMTOOLS_SH="_loop_samtools_only_20180219.sh"; echo ${SAMTOOLS_SH}
-# MINIMAP2_BSUB=""; echo ${MINIMAP2_BSUB}
-# MINIMAP2_SH=""; echo ${MINIMAP2_SH}
+MINIMAP2_BSUB="loop_minimap2_only_20180218.bsub"; echo ${MINIMAP2_BSUB}
+MINIMAP2_SH="_loop_minimap2_only_20180218.sh"; echo ${MINIMAP2_SH}
+# SAMTOOLS_BSUB="loop_samtools_only_20180219.bsub"; echo ${SAMTOOLS_BSUB}
+# SAMTOOLS_SH="_loop_samtools_only_20180219.sh"; echo ${SAMTOOLS_SH}
 
 cd ~/greenland_2017/platesA2B2/platesA2B2_combined; ls
-# parallel cp ${MINIMAP2_BSUB} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-# parallel cp ${MINIMAP2_SH} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-parallel cp ${SAMTOOLS_BSUB} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-parallel cp ${SAMTOOLS_SH} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp ${MINIMAP2_BSUB} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+parallel cp ${MINIMAP2_SH} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+# parallel cp ${SAMTOOLS_BSUB} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
+# parallel cp ${SAMTOOLS_SH} BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp loop_trimgalore_20180216.bsub BWA{} ::: 01 02 03 04 05 06 07 08 09 10
 # parallel cp _loop_trimgalore_20180216.sh BWA{} ::: 01 02 03 04 05 06 07 08 09 10
-ls BWA{01,02,03,04,05,06,07,08,09,10} #| tail -n 2
+ls BWA{01,02,03,04,05,06,07,08,09,10}
 
 # edit the bsub files so that the correct job name will show up (i suppose i could have instead run a job array...)
 cd ~/greenland_2017/platesA2B2/platesA2B2_combined; ls
-# parallel "sed 's/mnmploop01/mnmploop{}/g' BWA{}/loop_minimap2_only_20180218.bsub > BWA{}/loop_minimap2_only_20180218_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-# parallel "mv BWA{}/loop_minimap2_only_20180218_tmp.bsub BWA{}/loop_minimap2_only_20180218.bsub" ::: 01 02 03 04 05 06 07 08 09 10
-# head -n5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
-# tail -n5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
+parallel "sed 's/mnmploop01/mnmploop{}/g' BWA{}/${MINIMAP2_BSUB} > BWA{}/${MINIMAP2_BSUB}_tmp" ::: 01 02 03 04 05 06 07 08 09 10
+parallel "mv BWA{}/${MINIMAP2_BSUB}_tmp BWA{}/${MINIMAP2_BSUB}" ::: 01 02 03 04 05 06 07 08 09 10
+head -n5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
+     # check if i'm using mellanox-ib
+tail -n2 BWA{01,02,03,04,05,06,07,08,09,10}/loop_minimap2_only_20180218.bsub # check.  should be mnmploop10
 
-parallel "sed 's/samtools01/smtlA2B2{}/g' BWA{}/${SAMTOOLS_BSUB} > BWA{}/${SAMTOOLS_BSUB}_tmp" ::: 01 02 03 04 05 06 07 08 09 10
-parallel "mv BWA{}/${SAMTOOLS_BSUB}_tmp BWA{}/${SAMTOOLS_BSUB}" ::: 01 02 03 04 05 06 07 08 09 10
-head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/${SAMTOOLS_BSUB} # check.  should have the correct index number
-tail -n 1 BWA{01,02,03,04,05,06,07,08,09,10}/${SAMTOOLS_BSUB} # check.  should have the correct samtools shell filename
+# parallel "sed 's/samtools01/smtlA2B2{}/g' BWA{}/${SAMTOOLS_BSUB} > BWA{}/${SAMTOOLS_BSUB}_tmp" ::: 01 02 03 04 05 06 07 08 09 10
+# parallel "mv BWA{}/${SAMTOOLS_BSUB}_tmp BWA{}/${SAMTOOLS_BSUB}" ::: 01 02 03 04 05 06 07 08 09 10
+# head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/${SAMTOOLS_BSUB} # check.  should have the correct index number
+# tail -n 1 BWA{01,02,03,04,05,06,07,08,09,10}/${SAMTOOLS_BSUB} # check.  should have the correct samtools shell filename
 ls # BWA* folders should now sort to bottom
 
 # parallel "sed 's/trimgal01/trimgal{}/g' BWA{}/loop_trimgalore_20180216.bsub > BWA{}/loop_trimgalore_20180216_tmp.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "mv BWA{}/loop_trimgalore_20180216_tmp.bsub BWA{}/loop_trimgalore_20180216.bsub" ::: 01 02 03 04 05 06 07 08 09 10
 # head -n 5 BWA{01,02,03,04,05,06,07,08,09,10}/loop_trimgalore_20180216.bsub # check job names
+
+####### launch the minimap2 scripts #######
+# cd into each BWA folder and submit bsub job
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA01; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA02; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA03; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA04; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA05; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA06; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA07; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA08; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA09; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
+
+cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA10; ls
+bsub < ${MINIMAP2_BSUB}
+bjobs
+ls
 
 ####### launch samtools scripts #######
 cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA01; ls
@@ -94,58 +147,6 @@ cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA10; ls
 bsub < ${SAMTOOLS_BSUB}
 bjobs | sort -k8
 ls minimap2_outputs # should show new bam files
-
-####### launch the minimap2 scripts #######
-# cd into each BWA folder and submit bsub job
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA01; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA02; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA03; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA04; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA05; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA06; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA07; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA08; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA09; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs
-ls
-
-cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA10; ls
-bsub < loop_minimap2_only_20180218.bsub
-bjobs | sort -k8
-ls
 
 ####### launch trimgalore scripts #######
 cd ~/greenland_2017/platesA2B2/platesA2B2_combined/BWA01; ls
